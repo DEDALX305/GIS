2c7172d33bc20558ff1bb2339fa762f81c89e99e
DEDALX305 <dedalx305@mail.ru>
2017-03-27 22:06:30 +0500

    1#

--- 2Lab/Requests/Requests/Program.cs
+++ 2Lab/Requests/Requests/Program.cs
@@ -11,16 +11,16 @@ namespace Requests
 {
     class Program
     {
-        public static string text, zapros;
+        public static string text, Request;
         static void Main(string[] args)
         {
             for (int i = 1; i <= 500; i++)
             {
-                zapros = Console.ReadLine();
+                Request = Console.ReadLine();
 
                 string url2 = "https://search-maps.yandex.ru/v1/?text=";
                 string url3 = "&results=500&lang=ru_RU&apikey=1bf0d4d0-914f-48d9-95a0-05f771f66ae1";
-                string url4 = url2 + zapros + url3;
+                string url4 = url2 + Request + url3;
                 // Адрес ресурса, к которому выполняется запрос
                 // string url = "https://search-maps.yandex.ru/v1/?text=dw&type=biz&results=500&lang=ru_RU&apikey=1bf0d4d0-914f-48d9-95a0-05f771f66ae1";
                 string url = url4;
@@ -29,7 +29,7 @@ namespace Requests
                 using (var writer = new StreamWriter("Запросы.txt", true))
                 {
                     //Добавляем к старому содержимому файла
-                    writer.WriteLine(zapros);
+                    writer.WriteLine(Request);
                 }
 
                 // Создаём объект WebClient
